76209b254b26109b71840ef62343de30942677f2
brandon-jesse <102050658+brandon-jesse@users.noreply.github.com>
2022-07-08 12:41:34 -0500

    node server

---
+++ script.js
@@ -0,0 +1,48 @@
+const express = require('express'),
+  app = express(),
+  fs = require('fs'),
+  shell = require('shelljs'),
+
+   // Modify the folder path in which responses need to be stored
+  folderPath = './Responses/',
+  defaultFileExtension = 'json', // Change the default file extension
+  bodyParser = require('body-parser'),
+  DEFAULT_MODE = 'appendFile',
+  path = require('path');
+
+// Create the folder path in case it doesn't exist
+shell.mkdir('-p', folderPath);
+
+ // Change the limits according to your response size
+app.use(bodyParser.json({limit: '500mb', extended: true}));
+app.use(bodyParser.urlencoded({ limit: '500mb', extended: true })); 
+
+app.get('/', (req, res) => res.send('Hello, I write data to file. Send them requests!'));
+
+app.post('/write', (req, res) => {
+  console.log(req.body);
+
+  let extension = req.body.fileExtension || defaultFileExtension,
+    fsMode = req.body.mode || DEFAULT_MODE,
+    uniqueIdentifier = req.body.uniqueIdentifier ? typeof req.body.uniqueIdentifier === 'boolean' ? Date.now() : req.body.uniqueIdentifier : false,
+    filename = `${req.body.requestName}${uniqueIdentifier || ''}`,
+    filePath = `${path.join(folderPath, filename)}.${extension}`,
+    options = req.body.options || undefined;
+
+ var post = fs.createWriteStream('./postman.json',{flags:'a+'}).on('error', function (err) {
+ 
+  console.log(err);
+
+});
+post.write(JSON.stringify(req.body)+"\r\n");
+
+res.send('file updated');
+res.send(req.body);
+post.end();
+
+});
+
+app.listen(3000, () => {
+  console.log('ResponsesToFile App is listening now! Send them requests my way!');
+  console.log(`Data is being stored at location: ${path.join(process.cwd(), folderPath)}`);
+});
